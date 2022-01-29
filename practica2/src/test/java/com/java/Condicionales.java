86d6388cca5f9c07065fa292a88995c946271c49
Samuel <Samuel@DESKTOP-0P18FIG>
2022-01-29 12:03:24 -0600

    Add Condicionales

---
+++ practica2/src/test/java/com/java/Condicionales.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+		//Automovil conduciendo a ecceso de velocidad
+		
+		//IF
+		//IF ELSE
+		//IF ANIDADO		
+		//IF ANIDADO		
+		//SWITCH CASE
+		
+		
+
+	}
+
+}
